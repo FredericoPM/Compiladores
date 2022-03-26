ba403bbccfb2cece4cca9368dad99f9613f4b875
Arthur Moura <69174018+ArthurB70@users.noreply.github.com>
2022-03-26 17:21:42 -0300

    leitura do arquivo

--- testes/Tester.java
+++ testes/Tester.java
@@ -1,13 +1,23 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Files;
+
 
 public class Tester {
 
 	public static void main(String[] args) throws IOException{
-		
+
+		File file = new File("testes/test.cl.skel");
+		String string_arquivo = new String(Files.readAllBytes(file.toPath()));
+
+
+		/* 		
 		String expr = "[ teste ]";
-		
 		AnalisadorLexico lexical = new AnalisadorLexico(new StringReader(expr));
+		*/
+
+		AnalisadorLexico lexical = new AnalisadorLexico(new StringReader(string_arquivo));
 		lexical.yylex();
 
 	}
